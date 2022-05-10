00882a380c7cb27237f0a5b1a2d051e3a606f3ed
Amina Hayat <hayat.amina@hotmail.com>
2022-05-10 20:28:03 +0100

    git commit

---
+++ questions.js
@@ -0,0 +1,106 @@
+// Questions and Answers
+
+const questions= [
+    {
+        questionText: "Which of the following statements can be used to terminate a loop,switch or label statement",
+        options: [
+            "1.break",
+            "2. stop",
+            "3. halt",
+            "4.exit",
+         ],
+        answer: "1.break",
+    },
+    {
+        questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            "1. JavaScript",
+            "2. terminal/bash",
+            "3. for loops",
+            "4. console.log",
+         ],
+        answer: "4. console.log",
+    },
+    {
+        questionText: "String values must be enclosed within _____ when being assigned to variables.",
+        options: [
+             "1. commas", 
+             "2. curly brackets", 
+             "3. quotes",
+              "4. parentheses",
+         ],
+        answer: "3. quotes",
+    },
+    {
+        questionText: "Inside which HTML element do we put the Javascript?",
+        options: [
+            "1.script",
+            "2.javascript",
+            "3.scripting",
+            "4.js",
+          
+         ],
+        answer: "1.script",
+    },
+    {
+        questionText: "Arrays in JavaScript can be used to store ______.",
+        options: [
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above",
+         ],
+        answer: "4. all of the above",
+    },
+    {
+        questionText: "Commonly used data types DO NOT include:",
+        options: [
+            "1. strings",
+            "2. booleans", 
+            "3. alerts", 
+            "4. numbers",
+         ],
+        answer: "3. alerts",
+    },
+    {
+        questionText: "",
+        options: [
+            "1.break",
+            "2. stop",
+            "3. halt",
+            "4.exit",
+         ],
+        answer: "1.break",
+    },
+    {
+        questionText: "What is the correct syntax for writing an 'IF' statement?",
+        options: [
+            "1.if i == 5 then",
+            "2.if i == 5",
+            "3.if i = 5 then",
+            "4.if (i == 5)",
+         ],
+        answer: "4.if (i == 5)",
+    },
+    {
+        questionText: "Which operator is used to assign a value to a variable",
+        options: [
+            "1. *",
+            "2. -",
+            "3. +",
+            "4. =",
+         ],
+        answer: "4. =",
+    },
+    {
+        questionText: "Where is the best practice for placing your JS script?",
+        options: [
+            "1.At the beginning of the head",
+            "2.At the beginning of the body",
+            "3.At the end of the body",
+            "4.At the end of the head",
+         ],
+        answer: "3.At the end of the body",
+    },
+    
+]
